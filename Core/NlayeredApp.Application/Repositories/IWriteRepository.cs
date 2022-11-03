177a85bab94a0bd98451deb338dc468a22eb8016
byndr <byndr28@gmail.com>
2022-11-03 14:04:17 +0300

    Repositories

---
+++ Core/NlayeredApp.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlayeredApp.Application.Repositories
+{
+    public interface IWriteRepository<T>: IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> Get(int id);
+    }
+}
